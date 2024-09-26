baf0995016a3fcf5e5e8964fc4b9d3ae35608419
jaherndz <JulioAHernandez@protonmail.com>
2024-09-25 22:32:50 -0500

    adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+//Author Julio Hernandez <jhernandez32@unomaha.edu>
 #include<iostream>  
   
 using namespace std; 
